edc0d7f45b7c143aebf815404bd6c474ae277c57
Mohan <Getmohanraj95@gmail.com>
2019-06-03 22:35:29 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var url = require('url');
+
+const urlObj = url.parse(req.url)
 
 var port = process.env.PORT || 8080;
 
@@ -13,6 +16,9 @@ app.get('/', function(req, res) {
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=token&client_id='+ req.query.client_id);
 	}
 	else {
+		console.log(urlObj);
+		console.log(urlObj.hash);
+		console.log(req.url);
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+req.query.state+'&access_token= '+ req.query.access_token +'&token_type=Bearer');
 	}
 });
