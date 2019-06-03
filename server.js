4cc7068c3c2ace41294ce118af3dd5df82bcbbdd
Mohan <Getmohanraj95@gmail.com>
2019-06-03 22:39:08 +0530

    second commit

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var path = require('path');
 var url = require('url');
 
-const urlObj = url.parse(req.url)
+
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-
+	const urlObj = url.parse(req.url)
 	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com') {
 
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=token&client_id='+ req.query.client_id);
