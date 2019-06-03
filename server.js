ea7b8a9d8b8c50e155ee2a5306c84eeef86eb415
Mohan <Getmohanraj95@gmail.com>
2019-06-03 22:29:16 +0530

    second commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
 
 	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com') {
 
-		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&state='+ req.query.state+'&redirect_uri=https://macdealexa.co.in/gitkraken&response_type=token&client_id='+ req.query.client_id);
+		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=token&client_id='+ req.query.client_id);
 	}
 	else {
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+req.query.state+'&access_token= '+ req.query.access_token +'&token_type=Bearer');
