89a81b27a696d550529a018bf45e45e6669cf5fa
Mohan <Getmohanraj95@gmail.com>
2019-06-06 00:50:16 +0530

    last commit apost

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
+const jsonfile = require('jsonfile')
 var path = require('path');
 var url = require('url');
 
@@ -31,7 +33,14 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-	const urlObj = url.parse(req.url)
+
+		// let fileName = './data.json';
+
+		// let file = jsonfile.readFileSync(fileName);
+
+		// file.data = req.query.code;
+
+	    // jsonfile.writeFileSync(fileName,file);
 
 		res.render('index');
 		//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
@@ -48,3 +57,5 @@ app.get('/app', function(req, res) {
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 })
+
+
