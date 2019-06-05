fb5d9d7a8d37b503b022ca651855195cea9fca84
Mohan <Getmohanraj95@gmail.com>
2019-06-06 00:32:17 +0530

    last commit apost

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
 	const urlObj = url.parse(req.url)
-	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && req.query.code) {
+	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' ) {
 
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=token&client_id='+ req.query.client_id);
 	}
-	if(req.query.client_id  && req.query.code) {
+	// if(req.query.client_id  && req.query.code) {
 
-		res.redirect('https://accounts.google.com/o/oauth2/revoke?token='+ req.query.code);
-	}
+	// 	res.redirect('https://accounts.google.com/o/oauth2/revoke?token='+ req.query.code);
+	// }
 	else {
 
 		res.render('index');
@@ -37,6 +37,14 @@ app.post('/', function(req, res) {
 		//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
 
 });
+
+app.get('/app', function(req, res) {
+
+
+	res.json({state : req.query.state, code : req.query.code});
+
+});
+
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 })
