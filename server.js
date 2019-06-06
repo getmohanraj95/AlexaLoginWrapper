1fd38cbb49cc84b4e414df23bacfb2ce7b231693
Mohan <Getmohanraj95@gmail.com>
2019-06-07 00:18:21 +0530

    last

--- server.js
+++ server.js
@@ -23,35 +23,23 @@ app.get('/', function(req, res) {
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&access_type=offline&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=code&client_id='+ req.query.client_id);
 	}
 
+	else if(req.query.access_token) {
+
+		console.log("get 4")
+		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
+
+
+	}
+
 	else if(req.query.code) {
 		console.log("get 2")
 		var origin = req.get('origin');
 		console.log(origin);
-		//console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
-		// if(req.query.access_token) {
-		// 	console.log('inside 2 token');
-		// 	console.log(req.body);
-		// 	res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
-		// }
+
 		//res.redirect('https://alexa-login-wrapper.herokuapp.com?state='+req.query.state + '&code=' + req.query.code);
 		//res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
 
-		var options = { method: 'POST',
-		url: 'http://www.googleapis.com/oauth2/v4/token',
-		qs: { code: '4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA' },
-		headers:
-		{ 'Postman-Token': '659a66e2-95a1-4306-a720-d92b93b3b34d',
-			'cache-control': 'no-cache',
-			'content-type': 'application/x-www-form-urlencoded' } };
 
-		request(options, function (error, response, body) {
-		if (error) throw new Error(error);
-			ab = body;
-		console.log(body);
-
-		});
-
-		res.send(ab);
 	}
 
 	else if(req.query.state) {
@@ -61,20 +49,14 @@ app.get('/', function(req, res) {
 
 	}
 
-	else if(req.query.access_token) {
 
-		console.log("get 4")
-		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
-
-
-	}
 
 });
 
 app.post('/', function(req, res) {
 
 	console.log("post 1")
-	console.log(req.body);
+
 	//res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
 	res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
 	//res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
@@ -102,6 +84,22 @@ app.listen(port, function() {
 
 
 
+// var options = { method: 'POST',
+// 		url: 'http://www.googleapis.com/oauth2/v4/token',
+// 		qs: { code: '4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA' },
+// 		headers:
+// 		{ 'Postman-Token': '659a66e2-95a1-4306-a720-d92b93b3b34d',
+// 			'cache-control': 'no-cache',
+// 			'content-type': 'application/x-www-form-urlencoded' } };
+
+// 		request(options, function (error, response, body) {
+// 		if (error) throw new Error(error);
+// 			ab = body;
+// 		console.log(body);
+
+// 		});
+
+// 		res.send(ab);
 
 // var options = { method: 'POST',
 // 		url: 'http://www.googleapis.com/oauth2/v4/token',
