3454c0be96461d8cf2f715f5f5b97210a16a8643
Mohan <Getmohanraj95@gmail.com>
2019-06-07 00:38:20 +0530

    last

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
+var datafile = require('./data.json');
 var jsonfile = require('jsonfile');
 var path = require('path');
 var url = require('url');
@@ -15,7 +15,8 @@ app.engine('html', require('ejs').renderFile);
 
 
 app.get('/', function(req, res) {
-
+	console.log(data.data);
+	res.send('helo mhha')
 	const urlObj = url.parse(req.url)
 
 	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && !req.query.client_secret ) {
@@ -26,6 +27,8 @@ app.get('/', function(req, res) {
 	else if(req.query.access_token) {
 
 		console.log("get 4")
+		console.log(req.body);
+
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
 
 
@@ -34,19 +37,31 @@ app.get('/', function(req, res) {
 	else if(req.query.code) {
 		console.log("get 2")
 
+		var a = datafile.data + 1;
 
-		res.redirect('https://alexa-login-wrapper.herokuapp.com?state='+req.query.state + '&code=' + req.query.code);
+		let file = jsonfile.readFileSync(datafile);
+
+		file.data =  a ;
+
+
+		jsonfile.writeFileSync(datafile,file);
+		if(req.query.code && data.data === 2) {
+			res.redirect('www.googleapis.com/oauth2/v4/token?code='+ req.query.code +'&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=' + req.query.code);
+		}
+
+
+		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 		//res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
 
 
 	}
 
-	else if(req.query.state) {
+	// else if(req.query.state) {
 
-		console.log("get 3")
-		res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
+	// 	console.log("get 3")
+	// 	res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
 
-	}
+	// }
 
 
 
