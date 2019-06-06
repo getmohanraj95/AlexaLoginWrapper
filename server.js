e8e7099cf82005518e815f8edd635e6fe407e9c1
Mohan <Getmohanraj95@gmail.com>
2019-06-06 22:56:16 +0530

    last

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.get('/', function(req, res) {
 	const urlObj = url.parse(req.url)
 
 	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && !req.query.client_secret ) {
-
+		console.log("get 1")
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&access_type=offline&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=code&client_id='+ req.query.client_id);
 	}
 
 	if(req.query.code) {
-
+		console.log("get 2")
 		console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
@@ -31,14 +31,14 @@ app.get('/', function(req, res) {
 
 	if(req.query.state) {
 
-
+		console.log("get 3")
 		res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
 
 	}
 
 	if(req.query.access_token) {
 
-
+		console.log("get 4")
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
 
 
@@ -47,9 +47,9 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-	console.log(req.query)
+	console.log("post 1")
 	res.redirect('https://macdealexa.co.in');
-		//res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
+	//res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
 
 });
 
@@ -60,20 +60,3 @@ app.listen(port, function() {
 })
 
 
-
-
-
-
-
-
-// app.get('/app', function(req, res) {
-
-// 	if(req.query.code) {
-
-// 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
-// 		//res.render('index');
-// 		//res.send('index');
-// 	}
-
-
-// });
