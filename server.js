48eb7424a6c0b97a970fe29069b4f5b3497e9b02
Mohan <Getmohanraj95@gmail.com>
2019-06-06 21:51:14 +0530

    last

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var app = express();
-const fs = require('fs');
-const jsonfile = require('jsonfile')
+var fs = require('fs');
+var jsonfile = require('jsonfile');
 var path = require('path');
 var url = require('url');
-
-
-
 var port = process.env.PORT || 8080;
 
 app.set('views', path.join(__dirname, 'views'));
@@ -16,8 +13,10 @@ app.engine('html', require('ejs').renderFile);
 
 
 app.get('/', function(req, res) {
+
 	const urlObj = url.parse(req.url)
-	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' ) {
+
+	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && !req.query.code ) {
 
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&access_type=offline&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=code&client_id='+ req.query.client_id);
 	}
@@ -27,36 +26,25 @@ app.get('/', function(req, res) {
 	// }
 	if(req.query.code) {
 
+		console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
+
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
-		//res.render('index');
-		//res.send('index');
+
 	}
 	else {
 
 		res.render('index');
 		//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
 	}
+
 });
 
 app.post('/', function(req, res) {
 
-		// let fileName = './data.json';
+	//res.render('index');
+	//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
 
-		// let file = jsonfile.readFileSync(fileName);
-
-		// file.data = req.query.code;
-
-	    // jsonfile.writeFileSync(fileName,file);
-
-		res.render('index');
-		//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
-
-});
-
-app.get('/app', function(req, res) {
-
-
-	res.render('index');
+	res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 
 });
 
@@ -65,3 +53,20 @@ app.listen(port, function() {
 })
 
 
+
+
+
+
+
+
+// app.get('/app', function(req, res) {
+
+// 	if(req.query.code) {
+
+// 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
+// 		//res.render('index');
+// 		//res.send('index');
+// 	}
+
+
+// });
