a065d4decd7b61ea20176af192faef1a312d3c7a
Mohan <Getmohanraj95@gmail.com>
2019-06-06 23:24:16 +0530

    last

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var jsonfile = require('jsonfile');
 var path = require('path');
 var url = require('url');
 var port = process.env.PORT || 8080;
+var request = require("request");
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,7 +27,25 @@ app.get('/', function(req, res) {
 		//console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 
 		//res.redirect('https://alexa-login-wrapper.herokuapp.com?state='+req.query.state + '&code=' + req.query.code);
-		res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
+		//res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
+		var ab ;
+
+		var options = { method: 'POST',
+		url: 'http://www.googleapis.com/oauth2/v4/token',
+		qs: { code: '4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA' },
+		headers:
+		{ 'Postman-Token': '659a66e2-95a1-4306-a720-d92b93b3b34d',
+			'cache-control': 'no-cache',
+			'content-type': 'application/x-www-form-urlencoded' } };
+
+		request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+			ab = body;
+		console.log(body);
+
+		});
+
+		res.send(ab);
 
 	}
 
