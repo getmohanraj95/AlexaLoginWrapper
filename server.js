877dbf1efe83134e98762ae8d42e5612c7236c16
Mohan <Getmohanraj95@gmail.com>
2019-06-06 23:36:54 +0530

    last

--- server.js
+++ server.js
@@ -26,26 +26,11 @@ app.get('/', function(req, res) {
 		console.log("get 2")
 		//console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 
-		//res.redirect('https://alexa-login-wrapper.herokuapp.com?state='+req.query.state + '&code=' + req.query.code);
+		res.redirect('https://alexa-login-wrapper.herokuapp.com?state='+req.query.state + '&code=' + req.query.code);
 		//res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
-		var ab ;
 
-		var options = { method: 'POST',
-		url: 'http://www.googleapis.com/oauth2/v4/token',
-		qs: { code: '4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA' },
-		headers:
-		{ 'Postman-Token': '659a66e2-95a1-4306-a720-d92b93b3b34d',
-			'cache-control': 'no-cache',
-			'content-type': 'application/x-www-form-urlencoded' } };
 
-		request(options, function (error, response, body) {
-		if (error) throw new Error(error);
-			ab = body;
-		console.log(body);
 
-		});
-
-		res.send(ab);
 
 	}
 
@@ -67,8 +52,9 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
+
 	console.log("post 1")
-	console.log(req.query);
+	console.log(req.body);
 	res.redirect('www.googleapis.com/oauth2/v4/token?code=4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA#&client_id=828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com&client_secret=ipJjuOPeW4kwdQvLFiPBnVq9&redirect_uri=https://alexa-login-wrapper.herokuapp.com&grant_type=authorization_code');
 	//res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
 
@@ -81,3 +67,34 @@ app.listen(port, function() {
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var options = { method: 'POST',
+// 		url: 'http://www.googleapis.com/oauth2/v4/token',
+// 		qs: { code: '4/YQHgNlELY8MdSvhg1MyHvRi8A8-WefjuEsfsoKjtemC2e0_WSMUrHSuq0E_s09p0-We7z4wnca6JSmi6yD2xUZA' },
+// 		headers:
+// 		{ 'Postman-Token': '659a66e2-95a1-4306-a720-d92b93b3b34d',
+// 			'cache-control': 'no-cache',
+// 			'content-type': 'application/x-www-form-urlencoded' } };
+
+// 		request(options, function (error, response, body) {
+// 		if (error) throw new Error(error);
+// 			ab = body;
+// 		console.log(body);
+
+// 		});
+
+// 		res.send(ab);
