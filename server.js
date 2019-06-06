6f36633459646184da744bb6b57618c36eed077d
Mohan <Getmohanraj95@gmail.com>
2019-06-06 22:03:18 +0530

    last

--- server.js
+++ server.js
@@ -16,14 +16,11 @@ app.get('/', function(req, res) {
 
 	const urlObj = url.parse(req.url)
 
-	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && !req.query.code ) {
+	if(req.query.client_id ==='828225850885-kaka1030svjtfjtjfndpmm13mjkpb2d0.apps.googleusercontent.com' && !req.query.client_secret ) {
 
 		res.redirect('https://accounts.google.com/o/oauth2/v2/auth?scope=' + req.query.scope+' &include_granted_scopes=true&access_type=offline&state='+ req.query.state+'&redirect_uri=https://alexa-login-wrapper.herokuapp.com&response_type=code&client_id='+ req.query.client_id);
 	}
-	// if(req.query.client_id  && req.query.code) {
 
-	// 	res.redirect('https://accounts.google.com/o/oauth2/revoke?token='+ req.query.code);
-	// }
 	if(req.query.code) {
 
 		console.log('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
@@ -31,11 +28,27 @@ app.get('/', function(req, res) {
 		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO?state='+req.query.state + '&code=' + req.query.code);
 
 	}
-	else {
 
-		res.render('index');
-		//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
+	if(req.query.client_secret) {
+
+
+			res.redirect('https://accounts.google.com/o/oauth2/v4/token?code='+req.query.code+'&client_id='+ req.query.client_id + '&client_secret=' + req.query.client_secret +'&redirect_uri=https://alexa-login-wrapper.herokuapp.com'+'grant_type=authorization_code' );
+
 	}
+
+	if(req.query.access_token) {
+
+
+		res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO');
+//?access_token='+ req.query.access_token + '&refresh_token'
+
+	}
+
+	// else {
+
+	// 	res.render('index');
+	// 	//res.redirect('https://layla.amazon.com/api/skill/link/M28YSGJKH151MO#state='+urlObj.hash.state+'&access_token= '+ urlObj.hash.access_token +'&token_type=Bearer');
+	// }
 
 });
 
